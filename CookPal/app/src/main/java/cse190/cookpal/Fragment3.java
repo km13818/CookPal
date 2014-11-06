927e693b3575898d16d03ead134dc7e5f14bb58b
Sherry Cha <s.cha31@gmail.com>
2014-11-06 12:20:50 -0800

    basic nav drawer and action bar

---
+++ CookPal/app/src/main/java/cse190/cookpal/Fragment3.java
@@ -0,0 +1,42 @@
+package cse190.cookpal;
+
+/**
+ * Created by scha on 11/6/14.
+ */
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Fragment3 extends Fragment {
+
+    ImageView ivIcon;
+    TextView tvItemName;
+
+    public static final String IMAGE_RESOURCE_ID = "iconResourceID";
+    public static final String ITEM_NAME = "itemName";
+
+    public Fragment3() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.layout_3, container,
+                false);
+
+        ivIcon = (ImageView) view.findViewById(R.id.frag3_icon);
+        tvItemName = (TextView) view.findViewById(R.id.frag3_text);
+
+        tvItemName.setText(getArguments().getString(ITEM_NAME));
+        ivIcon.setImageDrawable(view.getResources().getDrawable(
+                getArguments().getInt(IMAGE_RESOURCE_ID)));
+        return view;
+    }
+
+}
