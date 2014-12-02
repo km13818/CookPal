388e9c3fb49e86a7ef5d46e2ef04ec296c508347
Tim Chi <timmy.g.chi@gmail.com>
2014-12-01 22:45:51 -0800

    working on sharing on fb

--- CookPal/app/src/main/java/cse190/cookpal/ShareActivity.java
+++ CookPal/app/src/main/java/cse190/cookpal/ShareActivity.java
@@ -1,17 +1,31 @@
 package cse190.cookpal;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.FacebookError;
+
 
 public class ShareActivity extends BaseDrawerActivity {
+
+    Facebook facebookClient;
+    SharedPreferences mPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        //loginToFacebook();
+
+        //if (facebookClient.isSessionValid()) {
+            postToWall();
+        //}
     }
 
 
@@ -32,5 +46,94 @@ public class ShareActivity extends BaseDrawerActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        facebookClient.authorizeCallback(requestCode, resultCode, data);
+    }
+
+    public void loginToFacebook() {
+        mPrefs = getPreferences(MODE_PRIVATE);
+        String access_token = mPrefs.getString("access_token", null);
+        long expires = mPrefs.getLong("access_expires", 0);
+
+        if (access_token != null) {
+            facebookClient.setAccessToken(access_token);
+        }
+
+        if (expires != 0) {
+            facebookClient.setAccessExpires(expires);
+        }
+
+        if (!facebookClient.isSessionValid()) {
+            facebookClient.authorize(this, new String[] { "publish_stream" }, new Facebook.DialogListener() {
+
+                @Override
+                public void onCancel() {
+                    // Function to handle cancel event
+                }
+
+                @Override
+                public void onComplete(Bundle values) {
+                    // Function to handle complete event
+                    // Edit Preferences and update facebook acess_token
+                    SharedPreferences.Editor editor = mPrefs.edit();
+                    editor.putString("access_token", facebookClient.getAccessToken());
+                    editor.putLong("access_expires", facebookClient.getAccessExpires());
+                    editor.commit();
+
+                    postToWall();
+                }
+
+                @Override
+                public void onError(DialogError error) {
+                    // Function to handle error
+
+                }
+
+                @Override
+                public void onFacebookError(FacebookError fberror) {
+                    // Function to handle Facebook errors
+
+                }
+
+            });
+        }
+    }
+
+    private void postToWall() {
+        Bundle parameters = new Bundle();
+        parameters.putString("name", "Battery Monitor");
+        parameters.putString("link", "https://play.google.com/store/apps/details?id=com.ck.batterymonitor");
+        parameters.putString("picture", "link to the picture");
+        parameters.putString("display", "page");
+        // parameters.putString("app_id", "228476323938322");
+
+        facebookClient.dialog(ShareActivity.this, "feed", parameters, new Facebook.DialogListener() {
+
+            @Override
+            public void onFacebookError(FacebookError e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onError(DialogError e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onComplete(Bundle values) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onCancel() {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 }
