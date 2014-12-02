47733a2e649acc8100d5e36566b3ebe3ba0845aa
Tim Chi <timmy.g.chi@gmail.com>
2014-12-01 16:53:38 -0800

    creating recipeActivity

--- CookPal/app/src/main/java/cse190/cookpal/RecipeActivity.java
+++ CookPal/app/src/main/java/cse190/cookpal/RecipeActivity.java
@@ -5,19 +5,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class ShareActivity extends BaseDrawerActivity {
+public class RecipeActivity extends BaseDrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_share);
+        setContentView(R.layout.activity_recipe);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.share, menu);
+        getMenuInflater().inflate(R.menu.recipe, menu);
         return true;
     }
 
