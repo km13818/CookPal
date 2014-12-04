e6c26c2ea0f52a49cd890846c3385bd20dc310ad
Tim Chi <timmy.g.chi@gmail.com>
2014-12-04 00:49:09 -0800

    populating directions page with instructions and time

--- CookPal/app/src/main/java/cse190/cookpal/Recipe.java
+++ CookPal/app/src/main/java/cse190/cookpal/Recipe.java
@@ -1,6 +1,5 @@
 package cse190.cookpal;
 
-import java.sql.Time;
 import java.util.ArrayList;
 
 public class Recipe {
@@ -43,16 +42,16 @@ public class Recipe {
     // TODO: temporary step population until actual data is passed from Recipe Intent --> remove this
     public ArrayList<Step> dummyStepList() {
         ArrayList<Step> stepList = new ArrayList<Step>();
-        Step currStep;
-
-        for(int i = 0; i < 10; i++) {
-            currStep = new Step("Chop the onions " + i,
-                    "Take your knife. And chop the onions. Don't cry or you're banished " + i,
-                    new Time(1000 * i), i);
-
-            stepList.add(currStep);
-        }
-
+//        Step currStep;
+//
+//        for(int i = 0; i < 10; i++) {
+//            currStep = new Step("Chop the onions " + i,
+//                    "Take your knife. And chop the onions. Don't cry or you're banished " + i,
+//                    new Time(1000 * i), i);
+//
+//            stepList.add(currStep);
+//        }
+//
         return stepList;
     }
 
