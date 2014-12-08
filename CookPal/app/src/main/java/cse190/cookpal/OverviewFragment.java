dda5f8432d9383b1a5a841d783821e49dcd91aed
Tim Chi <timmy.g.chi@gmail.com>
2014-12-07 16:26:58 -0800

    finished with overview page

--- CookPal/app/src/main/java/cse190/cookpal/OverviewFragment.java
+++ CookPal/app/src/main/java/cse190/cookpal/OverviewFragment.java
@@ -1,6 +1,8 @@
 package cse190.cookpal;
 
-import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,22 +11,46 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.InputStream;
+import java.net.URL;
+
 /**
  * Created by timchi on 11/28/14.
  */
 public class OverviewFragment extends Fragment {
 
-    @SuppressLint("NewApi")
-    @Override
+    private Bitmap bitmap;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.overview_tab, container, false);
 
-        Recipe currRecipe = RecipeActivity.currentRecipe;
+        Recipe currRecipe = RecipeActivity.getCurrentRecipe();
+        final String imageURL = currRecipe.getImgUrl();
+        final ImageView recipeImage = (ImageView) rootView.findViewById(R.id.overview_recipe_image);
 
-        ImageView recipeImage = (ImageView) rootView.findViewById(R.id.overview_recipe_image);
-        recipeImage.setImageResource(R.drawable.lasagna);
+       new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    bitmap = BitmapFactory.decodeStream((InputStream) new URL(imageURL).getContent());
+                } catch (Exception e) {
+                    // log error
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                if (bitmap != null) {
+                    recipeImage.setImageBitmap(bitmap);
+                }
+            }
+
+        }.execute();
+
+
+        //recipeImage.setImageResource(R.drawable.lasagna);
 
         TextView recipeName = (TextView) rootView.findViewById(R.id.overview_recipe_name);
         recipeName.setText(currRecipe.getRecipeName());
